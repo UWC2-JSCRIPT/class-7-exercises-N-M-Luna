26f8b9f7c81238549605a012b605cffefec76f65
N. M. Luna <nat.luna.64@gmail.com>
2022-12-02 17:32:04 -0800

    add first part of contact form

--- contact-me.js
+++ contact-me.js
@@ -1,1 +1,20 @@
-// TODO
+document.addEventListener('DOMContentLoaded', function () {
+
+    //Form elements that start out hidden
+    const jobElements = document.querySelector('.jobElements')
+    const codeElements = document.querySelector('.codeElements')
+
+    //When the client changes the selection of contactReason select element
+    const reasonSelect = document.querySelector('#contactReason')
+    reasonSelect.addEventListener('change', (e) => {
+
+        //hide the inappropriate form elements, and
+        jobElements.classList.add('hidden')
+        codeElements.classList.add('hidden')
+
+        //un-hide the appropriate form elements.
+        const divClassSelector = `.${reasonSelect.value}Elements`
+        const formElements = document.querySelector(divClassSelector)
+        formElements.classList.remove(`hidden`)
+    })
+})
