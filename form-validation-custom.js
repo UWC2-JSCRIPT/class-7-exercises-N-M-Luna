1f6d7684cc3334134d4b5af47037681438d28f23
N. M. Luna <nat.luna.64@gmail.com>
2022-12-02 16:12:40 -0800

    turn in code fragments

--- form-validation-custom.js
+++ form-validation-custom.js
@@ -1,1 +1,21 @@
 // TODO
+document.addEventListener('DOMContentLoaded', function() {
+    const firstName = document.getElementById("first-name")
+    const lastName = document.getElementById("last-name")
+    const email = document.getElementById("email")
+    const form = document.querySelector('form')
+  
+    function checkValidation(){
+      if (firstName.value.length < 3) { // first name invalid
+      firstName.validity.valid = false
+      firstName.classList.remove('')
+      firstName.setCustomValidity('Your name is too short.')
+      firstName.reportValidity()
+    } else {
+      firstName.validity.valid = true
+    }
+  }
+  
+    form.addEventListener('submit', () => {})
+  
+  })
